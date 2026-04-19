e10bc0365a1d64a3ec00e4fd9c93510062cf89b1
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2026-04-19 18:49:47 -0400

    Add badges data for certification achievements

---
+++ badges.js
@@ -0,0 +1,10 @@
+export const badges = [
+  { id: 1, name: "Cisco Certified", img: "/images/badges/cisco.png", alt: "Cisco Networking" },
+  { id: 2, name: "Veeam VMCE", img: "/images/badges/veeam.png", alt: "Veeam Backup" },
+  { id: 3, name: "LPI Linux", img: "/images/badges/linux.png", alt: "Linux Admin" },
+  { id: 4, name: "Power BI Data", img: "/images/badges/pbi.png", alt: "Data Analytics" },
+  { id: 5, name: "Sophos Architect", img: "/images/badges/sophos.png", alt: "Security" },
+  { id: 6, name: "VMware vCloud", img: "/images/badges/vmware.png", alt: "Virtualization" },
+  { id: 7, name: "Fortinet NSE", img: "/images/badges/fortinet.png", alt: "Network Security" },
+  { id: 8, name: "Microsoft SQL", img: "/images/badges/sql.png", alt: "Database Admin" }
+];
