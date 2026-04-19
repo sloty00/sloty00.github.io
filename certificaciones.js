5cab340ef3f70ba79725f2e078eb9797190dd389
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2026-04-19 18:54:12 -0400

    Update certificaciones.js

--- certificaciones.js
+++ certificaciones.js
@@ -1,34 +1,30 @@
-export const certificaciones = [
+export const diplomas = [
   {
-    id: "cisco-diploma",
+    id: "cert-cisco",
     titulo: "Cisco Networking Academy",
-    institucion: "Cisco",
-    img_miniatura: "/images/certificados/thumb_cisco.png", // Miniatura para el carrusel
-    img_full: "/images/certificados/diploma_cisco_full.png", // El diploma real escaneado
-    descripcion: "Certificación oficial en configuración y administración de redes."
+    institucion: "Cisco Systems",
+    imagen: "/images/certificados/diploma-cisco.jpg",
+    categoria: "Infraestructura"
   },
   {
-    id: "veeam-diploma",
+    id: "cert-veeam",
     titulo: "Veeam Certified Engineer",
     institucion: "Veeam Software",
-    img_miniatura: "/images/certificados/thumb_veeam.png",
-    img_full: "/images/certificados/diploma_veeam_full.png",
-    descripcion: "Especialización en protección de datos e infraestructura inmutable."
+    imagen: "/images/certificados/diploma-veeam.jpg",
+    categoria: "Data Protection"
   },
   {
-    id: "pbi-diploma",
-    titulo: "Preparación de Datos con Power BI",
+    id: "cert-pbi",
+    titulo: "Power BI Data Preparation",
     institucion: "Microsoft Learn",
-    img_miniatura: "/images/certificados/thumb_pbi.png",
-    img_full: "/images/certificados/diploma_pbi_full.png",
-    descripcion: "Certificación técnica en modelado de datos y DAX."
+    imagen: "/images/certificados/diploma-pbi.jpg",
+    categoria: "Data Analytics"
   },
   {
-    id: "linux-diploma",
+    id: "cert-linux",
     titulo: "Linux Professional Institute",
     institucion: "LPI",
-    img_miniatura: "/images/certificados/thumb_linux.png",
-    img_full: "/images/certificados/diploma_linux_full.png",
-    descripcion: "Acreditación en administración de sistemas operativos Open Source."
+    imagen: "/images/certificados/diploma-linux.jpg",
+    categoria: "Sistemas"
   }
 ];
