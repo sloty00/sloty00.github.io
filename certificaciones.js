5f329ef48ce67fc5ca99556fa8eac29c6b20ab92
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2026-04-19 21:52:41 -0400

    Update certificaciones.js

--- certificaciones.js
+++ certificaciones.js
@@ -1,17 +1,38 @@
 export const diplomas = [
   {
-    id: "cert-cisco",
-    titulo: "Cisco Networking Academy",
-    institucion: "Cisco Systems",
-    imagen: "/images/certificados/diploma-cisco.jpg",
-    categoria: "Infraestructura"
+    id: "cert-cisco01",
+    titulo: "Cisco C++ Advanced",
+    institucion: "Cisco Systems/C++ Institute",
+    imagen: "/images/certificados/Cisco_C++_(Advanced).png",
+    categoria: "Desarrollo"
   },
   {
-    id: "cert-veeam",
-    titulo: "Veeam Certified Engineer",
-    institucion: "Veeam Software",
-    imagen: "/images/certificados/diploma-veeam.jpg",
-    categoria: "Data Protection"
+    id: "cert-cisco02",
+    titulo: "Cisco Hacker Etico",
+    institucion: "Cisco Systems",
+    imagen: "/images/certificados/Cisco_Hacker_Etico.png",
+    categoria: "Security"
+  },
+  {
+    id: "cert-cisco03",
+    titulo: "Cisco Network Technician",
+    institucion: "Cisco Systems/Universidad Tecnologica de Pereira",
+    imagen: "/images/certificados/Cisco_Network_Technician_Career_Path.png",
+    categoria: "Networking"
+  },
+  {
+    id: "cert-cisco04",
+    titulo: "Cisco Linux I",
+    institucion: "Cisco Systems",
+    imagen: "/images/certificados/Cisco_Linux_I.png",
+    categoria: "System"
+  },
+  {
+    id: "cert-cisco05",
+    titulo: "Cisco Linux II",
+    institucion: "Cisco Systems",
+    imagen: "/images/certificados/Cisco_Linux_I.png",
+    categoria: "System"
   },
   {
     id: "cert-pbi",
