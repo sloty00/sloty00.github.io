c5b065a5a12ea431b6e53c01cfa907f817391770
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2026-04-19 21:59:38 -0400

    Update certificaciones.js

--- certificaciones.js
+++ certificaciones.js
@@ -3,49 +3,49 @@ export const diplomas = [
     id: "cert-cisco01",
     titulo: "Cisco C++ Advanced",
     institucion: "Cisco Systems/C++ Institute",
-    imagen: "/images/certificados/Cisco_C++_(Advanced).png",
+    imagen: "/images/diplomas/Cisco_C++_(Advanced).png",
     categoria: "Desarrollo"
   },
   {
     id: "cert-cisco02",
     titulo: "Cisco Hacker Etico",
     institucion: "Cisco Systems",
-    imagen: "/images/certificados/Cisco_Hacker_Etico.png",
+    imagen: "/images/diplomas/Cisco_Hacker_Etico.png",
     categoria: "Security"
   },
   {
     id: "cert-cisco03",
     titulo: "Cisco Network Technician",
     institucion: "Cisco Systems/Universidad Tecnologica de Pereira",
-    imagen: "/images/certificados/Cisco_Network_Technician_Career_Path.png",
+    imagen: "/images/diplomas/Cisco_Network_Technician_Career_Path.png",
     categoria: "Networking"
   },
   {
     id: "cert-cisco04",
     titulo: "Cisco Linux I",
     institucion: "Cisco Systems",
-    imagen: "/images/certificados/Cisco_Linux_I.png",
+    imagen: "/images/diplomas/Cisco_Linux_I.png",
     categoria: "System"
   },
   {
     id: "cert-cisco05",
     titulo: "Cisco Linux II",
     institucion: "Cisco Systems",
-    imagen: "/images/certificados/Cisco_Linux_I.png",
+    imagen: "/images/diplomas/Cisco_Linux_I.png",
     categoria: "System"
   },
   {
     id: "cert-pbi",
     titulo: "Power BI Data Preparation",
     institucion: "Microsoft Learn",
-    imagen: "/images/certificados/diploma-pbi.jpg",
+    imagen: "/images/diplomas/diploma-pbi.jpg",
     categoria: "Data Analytics"
   },
   {
     id: "cert-linux",
     titulo: "Linux Professional Institute",
     institucion: "LPI",
-    imagen: "/images/certificados/diploma-linux.jpg",
+    imagen: "/images/diplomas/diploma-linux.jpg",
     categoria: "Sistemas"
   }
 ];
