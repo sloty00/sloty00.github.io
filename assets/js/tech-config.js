b1ce665207d8fc85a89048ef2b90d6060286c15b
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2026-04-28 17:28:24 -0400

    Create tech-config.js

---
+++ assets/js/tech-config.js
@@ -0,0 +1,16 @@
+// assets/js/tech-config.js
+const TECH_STACK_CONFIG = {
+    colors: {
+        "Nodejs": "339933", "Node.js": "339933", "React": "61DAFB", "Firebase": "FFCA28",
+        "Express": "000000", "Express.js": "000000", "JavaScript": "F7DF1E",
+        "Active Directory": "0078D4", "Networking": "00599C", "C++": "00599C",
+        "Cybersecurity": "E34F26", "Desktop Development": "0078D4" 
+        // ... agrega el resto aquí
+    },
+    logos: {
+        "Nodejs": "nodedotjs", "Node.js": "nodedotjs", "Active Directory": "microsoft",
+        "Networking": "cisco", "JavaScript": "javascript", "C++": "cplusplus",
+        "Cybersecurity": "securityscorecard", "Desktop Development": "windows"
+        // ... agrega el resto aquí
+    }
+};
